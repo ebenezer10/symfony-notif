93dca98807a7b89676b032a039984d93bb4058ac
ebenezer <nikaboue10@gmail.com>
2020-07-22 15:26:28 +0000

    Fully functional base

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\Publisher;
+use Symfony\Component\Mercure\PublisherInterface;
+use Symfony\Component\Mercure\Update;
+
+/**
+ * @IsGranted("ROLE_USER")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAll();
+        return $this->render('default/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/sendData/{user}", name="send_data")
+     */
+    public function __invoke(PublisherInterface $publisher,User $user): Response
+    {
+        $update = new Update(
+            'http://primus-notif-hub.com/user/'.$user->getId(),
+            json_encode(['status' => 'OutOfStock'])
+        );
+
+        // The Publisher service is an invokable object
+        $publisher($update);
+
+        return new Response('published!');
+    }
+}
